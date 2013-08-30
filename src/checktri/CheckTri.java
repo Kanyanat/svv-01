6e5cb8325a4288330a704e7eda03661068fcaeae
PoNd <PoNd@192.168.1.101>
2013-08-30 01:25:58 -0700

    pondjj

--- src/checktri/CheckTri.java
+++ src/checktri/CheckTri.java
@@ -3,8 +3,17 @@ package checktri;
 
 public class CheckTri {
 
-    public static String checkTriType(int p, int q) {
-        return null;
+    public static String checkTriType(int a, int b, int c) {
+        String triTypeString = "";
+        if(a==b && b==c && a==c ){
+            triTypeString = "Equilateral Triangle";
+        }else if(a==b || b==c || a==c ){
+            triTypeString = "Isosceles Triangle";
+        }else if(a!=b && b!=c && a!=c ){
+            triTypeString = "Scalene";
+        }else if(a<=0 || b<=0 || a<=0 )
+            triTypeString = "Invalid!!!";
+        return triTypeString;
 		
 	}
 }
