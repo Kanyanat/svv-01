516f400f9e4222a4a9d86b0212a1102d2d2e3d44
PoNd <PoNd@192.168.1.101>
2013-08-30 01:40:10 -0700

    pondjjq

--- src/checktri/CheckTri.java
+++ src/checktri/CheckTri.java
@@ -5,15 +5,19 @@ public class CheckTri {
 
     public static String checkTriType(int a, int b, int c) {
         String triTypeString = "";
-        if(a==b && b==c && a==c ){
-            triTypeString = "Equilateral Triangle";
-        }else if(a==b || b==c || a==c ){
-            triTypeString = "Isosceles Triangle";
-        }else if(a!=b && b!=c && a!=c ){
-            triTypeString = "Scalene";
-        }else if(a<=0 || b<=0 || a<=0 )
-            triTypeString = "Invalid!!!";
+        if (a > 0 && b > 0 && c > 0) {
+            if (a == b && b == c && a == c)
+                triTypeString = "Equilateral Triangle";
+            else if (a == b || b == c || a == c) 
+                triTypeString = "Isosceles Triangle";
+            else if (a != b && b != c && a != c) 
+                triTypeString = "Scalene";
+            else
+                triTypeString = "Invalid!!!";
+        }
+        if(a <= 0 || b <= 0 || a <= 0)
+                triTypeString = "Invalid!!!";
         return triTypeString;
-		
-	}
-}
+
+        }
+    }
